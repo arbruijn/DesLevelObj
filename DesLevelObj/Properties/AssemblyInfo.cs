1a3c03e615a6d7145157e335e3a9efb2f587074d
Arne de Bruijn <mail@arnedebruijn.nl>
2023-02-09 18:55:55 +0100

    Version v1.1.0

--- DesLevelObj/Properties/AssemblyInfo.cs
+++ DesLevelObj/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DesLevelObj")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Convert Descent 1/2/3 level meshes to .obj 3d files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Arne de Bruijn")]
 [assembly: AssemblyProduct("DesLevelObj")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © Arne de Bruijn 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.5.0")]
-[assembly: AssemblyFileVersion("0.8.5.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 GUI tool in C# to convert Descent 1/2/3 level meshes to .obj 3d files.
+
+# Changes
+
+2023-02-09 v1.1.0 \
+Added ability to remap textures with a json config file (by Jonathan Vukovich)
